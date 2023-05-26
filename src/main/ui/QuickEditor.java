a9d55800059208aaf3dd74fe0e33d7b3b485a5a1
Ellie <lattellie@gmail.com>
2023-05-26 12:32:01 -0700

    reactive piano color

--- src/main/ui/QuickEditor.java
+++ src/main/ui/QuickEditor.java
@@ -9,11 +9,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QuickEditor extends JFrame implements ActionListener, KeyListener {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 400;
-
+    private static final int[] NOTENUM = {49,81,50,87,51,52,82,53,84,54,89,55};
+    private ArrayList<Integer> activatedNotes;
+    //initialized to be all null, the last item(get 12, actual 13) is the rest
+    private Map<Integer,Integer> keyMap;
+    private int upDownPressed = 0;
+    // 1 if up is pressed, 0 if nothing pressed, -1 if down is pressed
+    private boolean isKeyPressed = false;
+    // to make sure there's no double detection
+    private boolean isUpDownPressed = false;
     private Score score;
     private JPanel panel;
     private PianoPanel pianoPanel;
@@ -27,11 +39,34 @@ public class QuickEditor extends JFrame implements ActionListener, KeyListener {
         setSize(WIDTH, HEIGHT);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         panel = new JPanel();
-        pianoPanel = new PianoPanel();
+        pianoPanel = new PianoPanel(this);
         add(panel, BorderLayout.SOUTH);
         add(pianoPanel, BorderLayout.CENTER);
+        addKeyListener(this);
         setMinimumSize(new Dimension(WIDTH, HEIGHT / 10));
         setVisible(true);
+        initializeKeyMap();
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new QuickEditor().setVisible(true));
+    }
+
+    public ArrayList<Integer> getActivatedNotes() {
+        return activatedNotes;
+    }
+
+    public int getUpDownPressed() {
+        return upDownPressed;
+    }
+
+    private void initializeKeyMap() {
+        keyMap = new HashMap<>();
+        activatedNotes = new ArrayList<>();
+        for (int i = 0; i < NOTENUM.length;i++) {
+            keyMap.put(NOTENUM[i],i);
+            activatedNotes.add(null);
+        }
+        activatedNotes.add(null);
     }
 
     @Override
@@ -39,9 +74,6 @@ public class QuickEditor extends JFrame implements ActionListener, KeyListener {
 
     }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new QuickEditor().setVisible(true));
-    }
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -50,11 +82,33 @@ public class QuickEditor extends JFrame implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (!isUpDownPressed && keyCode == 38 || keyCode == 40) {
+            isUpDownPressed = true;
+            if (Math.abs(upDownPressed + 39-keyCode) <= 3) {
+                upDownPressed += 39-keyCode;
+            }
+        }
+        if (!isKeyPressed && keyMap.get(keyCode) != null) {
+            isKeyPressed = true;
+            activatedNotes.set(keyMap.get(keyCode),upDownPressed);
+            Instant currentTimestamp = Instant.now();
+            // Print the current timestamp
+            System.out.println("Current timestamp: " + currentTimestamp);
+        }
+        repaint();
 
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+        int keyCode = e.getKeyCode();
+        if (keyCode == 38 || keyCode == 40) {
+            isUpDownPressed = false;
+        } else if (keyMap.get(keyCode) != null) {
+            isKeyPressed = false;
+            activatedNotes.set(keyMap.get(keyCode),null);
+        }
+        repaint();
     }
 }
--- src/main/ui/tools/PianoPanel.java
+++ src/main/ui/tools/PianoPanel.java
@@ -1,7 +1,10 @@
 package ui.tools;
 
+import ui.QuickEditor;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PianoPanel extends JPanel {
 
@@ -11,27 +14,51 @@ public class PianoPanel extends JPanel {
     private int keytop = 50;
     private int keyheight = 60;
     private int keywidth = 20;
-    public PianoPanel(){
+
+    private QuickEditor frame;
+    public PianoPanel(QuickEditor frame){
         super();
         setBackground(BG);
+        this.frame = frame;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        ArrayList<Integer> activatedNotes = frame.getActivatedNotes();
+        int octave = frame.getUpDownPressed();
         w = this.getWidth();
         h = this.getHeight();
         keyheight = h / 2;
         keywidth = w / 14;
         g.setColor(Color.white);
         int left = (int) (w/2-keywidth*3.5);
+        int sqwidth = keyheight / 7;
         for (int i=0; i<7; i++) {
+            if (i-3 == octave) {
+                g.setColor(Color.pink);
+            }
+            g.fillRect(left-sqwidth, keytop+sqwidth/5+(6-i)*sqwidth, sqwidth*4/5, sqwidth*4/5);
+            g.setColor(Color.white);
+        }
+        int[] whiteNode = new int[]{0,2,4,5,7,9,11};
+        int[] blackNode = new int[]{1,3,6,8,10};
+        int[] blackRect = new int[]{1,2,4,5,6};
+        for (int i=0; i<7; i++) {
+            if (activatedNotes.get(whiteNode[i]) != null) {
+                g.setColor(Color.PINK);
+            }
             g.fillRect(left + i * keywidth, keytop, (int) (keywidth * 0.9), keyheight);
+            g.setColor(Color.white);
         }
         g.setColor(Color.black);
         double ratio = 0.7;
-        for (int n : new int[]{1,2,4,5,6}) {
-            g.fillRect((int) (left - keywidth * ratio / 2 + n * keywidth), keytop, (int) (keywidth * ratio), keyheight*3/5);
+        for (int i=0; i<5; i++) {
+            if (activatedNotes.get(blackNode[i]) != null) {
+                g.setColor(Color.red);
+            }
+            g.fillRect((int) (left - keywidth * ratio / 2 + blackRect[i] * keywidth), keytop, (int) (keywidth * ratio), keyheight*3/5);
+            g.setColor(Color.black);
         }
     }
 }
