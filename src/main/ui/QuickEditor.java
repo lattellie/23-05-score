cffb14402c9db63ba9aa6aac3b271b30eadd231f
Ellie <lattellie@gmail.com>
2023-05-28 19:06:44 -0700

    test branch

--- src/main/ui/QuickEditor.java
+++ src/main/ui/QuickEditor.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 // TODO: set up rest
+// check if branch's made
 public class QuickEditor extends JFrame implements KeyListener {
     private static final int WIDTH = 600;
     private static final int HEIGHT = 400;
